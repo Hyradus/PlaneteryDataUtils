d3cc7e25393a96913da368f8c78dd90c2fd6a3c8
hyradus <giacomo.nodjoumi@hyranet.info>
2022-03-02 17:45:06 +0100

    added support for 8bit export, unified some functions, various fixes

--- PyISIS-Parallel/utils/KalaUtils.py
+++ PyISIS-Parallel/utils/KalaUtils.py
@@ -12,21 +12,21 @@ Created on Mon Sep 28 18:45:02 2020
 import os
 os.environ["ISISROOT"]="/opt/conda/envs/isis/"
 os.environ["ISISDATA"]="/isis/data"
-from kalasiris import lronac2isis, lrowac2isis, lronaccal, lrowaccal, chan1m32isis, cam2map, spiceinit, lronacecho
+from kalasiris import lronac2isis, lrowac2isis, lronaccal, lrowaccal, chan1m32isis, cam2map, spiceinit, lronacecho, stretch
 from osgeo import gdal
 from osgeo.gdal import gdalconst
 import rasterio as rio
 import time
+import subprocess
 
 def RAWtoL0(inst, src, L0):
-    inst(src, to=L0)
-    
+    inst(src, to=L0)    
 
-def L0toL1(dst_basename, L0, L1):
-    lronaccal(L0,to=L1)
+def L0toL1(inst, dst_basename, L0, L1):
+    inst(L0,to=L1)
     os.remove(L0)
 
-def L1toL1echo(L1,L1echo):
+def L1toL1nacecho(L1,L1echo):
     lronacecho(L1, to=L1echo)
     os.remove(L1)
     
@@ -36,14 +36,17 @@ def L1toL2(maptemplate, L1, L2):
     os.remove(L1)
 
     
-def L2toStd(L2cub, L2std):
+def L2toStd(L2cub, L2std, byte):
+    opts = f' -mask none'
+    
+    nodata=rio.open(L2cub).nodata #using rio since src_map.GetMetadata() return empty
+    if byte.lower() in ['yes','y','ye']:
+        nodata=0
+        opts = opts+f' -ot Byte  -scale -a_nodata {nodata}'            
+
     try:
         src_map = gdal.Open(L2cub)
-        nodata=rio.open(L2cub).nodata #using rio since src_map.GetMetadata() return empty
-        nodata=0
-        opts = f'-a_nodata {nodata} -mask none -scale -ot Byte'
-#        gdal.Translate(L2std, src_map,options=opts)
-        otype=gdalconst.GDT_Byte
+        
         gdal.Translate(L2std, src_map, options=opts)#,scaleParams=[])
         return 'Done'
         #os.remove(dst_lev2)
@@ -53,109 +56,138 @@ def L2toStd(L2cub, L2std):
 
 
 
-def lro(src, dst_basename, maptemplate, oxt, cam):
-    L2std = dst_basename+'_lev2.'+oxt
-    L2 = dst_basename+'_lev2.cub'
-    L1echo = dst_basename+'_lev1echo.cub'
+def lro(src, dst_basename, maptemplate, oxt, cam, byte):
+    L0 = dst_basename+'_lev0.cub'        
     L1 = dst_basename+'_lev1.cub'                
-    L0 = dst_basename+'_lev0.cub'    
-    if os.path.isfile(L2std):
-        print('File exists')
-    else:
-        L2 = L2std.split(oxt)[0]+'cub'
-        if os.path.isfile(L2):
-            L2toStd(L2, L2std)
+    L1echo = dst_basename+'_lev1echo.cub'
+    L2 = dst_basename+'_lev2.cub'
+    L2std = dst_basename+'_lev2.'+oxt
+    try:
+        if os.path.isfile(L2std):
+            print('File exists')
         else:
-            
-            if os.path.isfile(L1echo):
-                
-                L1toL2(maptemplate, L1echo, L2)
-                e=L2toStd(L2, L2std)
-            else:
-                
-                if os.path.isfile(L1):
-                    if cam in ['nac']:
-                        L1toL1echo(L1, L1echo)
-                        L1toL2(maptemplate, L1echo, L2)
-                    else:
-                        L1toL2(maptemplate, L1, L2)
-                    e=L2toStd(L2, L2std)
-                else:                        
-                    
-                    if os.path.isfile(L0):
-                        L0toL1(dst_basename, L0, L1)
+            L2 = L2std.split(oxt)[0]+'cub'
+            if os.path.isfile(L2):
+                L2toStd(L2, L2std, byte)
+            else:      
+                if os.path.isfile(L1echo):                
+                    L1toL2(maptemplate, L1echo, L2)
+                    L2toStd(L2, L2std, byte)
+                else:                
+                    if os.path.isfile(L1):
                         if cam in ['nac']:
-                            L1toL1echo(L1, L1echo)
+                            L1toL1nacecho(L1, L1echo)
                             L1toL2(maptemplate, L1echo, L2)
                         else:
-                            L1toL2(maptemplate, L1, L2)                                                
-                        e=L2toStd(L2, L2std)
-                    else:
-                        RAWtoL0(lronac2isis, src, L0)
-                        init = None
-                        while init == None:
-                            try:
-                                print('Spiceinit')
-                                spiceinit(L0)#, web='yes')
-                                init = 'Done'
-                                print(init)
-                            except subprocess.CalledProcessError as err:
+                            L1toL2(maptemplate, L1, L2)
+                        L2toStd(L2, L2std, byte)
+                    else:                                           
+                        if os.path.isfile(L0):
+                            if cam in ['nac']:
+                                L0toL1(lronaccal, dst_basename, L0, L1)
+                                L1toL1nacecho(L1, L1echo)
+                                L1toL2(maptemplate, L1echo, L2)
+                            else:
+                                L0toL1(lrowaccal, dst_basename, L0, L1)                            
+                                L1toL2(maptemplate, L1, L2)                                                
+                            L2toStd(L2, L2std, byte)
+                        else:
+                            if cam in ['nac']:
+                                RAWtoL0(lronac2isis, src, L0)
+                            else:
+                                RAWtoL0(lrowac2isis,src, L0)
+                            init = None
+                            while init == None:
+                                try:
+                                    print('Spiceinit')
+                                    spiceinit(L0)#, web='yes')
+                                    init = 'Done'
+                                    print(init)
+                                except subprocess.CalledProcessError as err:
+                                    print('Had an ISIS error:')
+                                    print(' '.join(err.cmd))
+                                    print(err.stdout)
+                                    print(err.stderr)
+                                    raise err
+
+                            if cam in ['nac']:
+                                L0toL1(lronaccal, dst_basename, L0, L1)
+                            else:
+                                L0toL1(lrowaccal, dst_basename, L0, L1)                        
+                            if cam in ['nac']:
+                                L1toL1nacecho(L1, L1echo)
+                                L1toL2(maptemplate, L1echo, L2)
+                            else:
+                                L1toL2(maptemplate, L1, L2)                                                
+                            L2toStd(L2, L2std, byte)
+    except subprocess.CalledProcessError as err:
+                        print('Had an ISIS error:')
+                        print(' '.join(err.cmd))
+                        print(err.stdout)
+                        print(err.stderr)
+                        raise err
+        
+                        
+
+
+
+def m3L1(src, dst_basename,maptemplate, oxt, cam, byte):
+    src_basename=src.split('_L1B.LBL')[0]
+    L1_L1B=src_basename+'_L1B.LBL'
+    L1_LOC=src_basename+'_LOC.IMG'
+    L1_OBS=src_basename+'_OBS.IMG'
+    L1 = dst_basename+'_lev1.cub'                        
+    L2str = dst_basename+'_lev2stretch.cub'        
+    L2 = dst_basename+'_lev2.cub'
+    L2std = dst_basename+'_lev2.'+oxt
+    
+    try:
+        if os.path.isfile(L2std):
+            print('File exists')
+        else:
+            L2 = L2std.split(oxt)[0]+'cub'
+            #if os.path.isfile(L2str):
+                #L2toStd(L2str, L2std, byte)
+            #else:            
+            if os.path.isfile(L2):
+                #null=rio.open(L2).nodata
+                #if byte.lower() in ['y','yes','ye']:
+                #    null=0
+                #stretch(L2, to=L2str, NULL=null)
+                L2toStd(L2, L2std, byte)               
+            else:
+                if os.path.isfile(L1):                    
+                    L1toL2(maptemplate, L1, L2)
+                    null=rio.open(L2).nodata
+            #        if byte.lower() in ['y','yes','ye']:
+            #            null=0
+            #        stretch(L2, to=L2str, NULL=null)
+                    L2toStd(L2, L2std, byte)
+
+                else:                                            
+                    chan1m32isis(L1_L1B, loc=L1_LOC, obs=L1_OBS, to=L1)
+                    init = None
+                    while init == None:
+                        print('Spiceinit')
+                        spiceinit(L1)#, web='yes')
+                        init = 'Done'
+                        print(init)
+                    print('Mapping')                        
+                    L1toL2(maptemplate, L1, L2)                    
+                    #null=rio.open(L2).nodata
+                    #if byte.lower() in ['y','yes','ye']:
+                    #    null=0
+                    #stretch(L2, to=L2str, NULL=null)
+                    print('Exporting')
+                    L2toStd(L2, L2std, byte)
+    except subprocess.CalledProcessError as err:
                                 print('Had an ISIS error:')
                                 print(' '.join(err.cmd))
                                 print(err.stdout)
                                 print(err.stderr)
                                 raise err
-                                
-                        L0toL1(dst_basename, L0, L1)
-                        if cam in ['nac']:
-                            L1toL1echo(L1, L1echo)
-                            L1toL2(maptemplate, L1echo, L2)
-                        else:
-                            L1toL2(maptemplate, L1, L2)                                                
-                        L2toStd(L2, L2std)
-                        e=L2toStd(L2, L2std)
-        return e
-                        
+    
 
-
-
-def m3(src, dst_basename,maptemplate):
-        src_basename=src.split('_L1B')[0]
-        src_L1B=src_basename+'_L1B.LBL'
-        src_LOC=src_basename+'_LOC.IMG'
-        src_OBS=src_basename+'_OBS.IMG'
-        dst_lev0 = dst_basename+'_lev0.cub'
-        if os.path.isfile(dst_lev0):
-            print('init cub exists')
-        else:
-            print('Creating cube')
-            chan1m32isis(src_L1B, loc=src_LOC, obs=src_OBS, to=dst_lev0)
-        init = None
-        while init == None:
-            try:
-                spiceinit(dst_lev0, web='yes')
-                init = 'Done'
-            except:
-                pass
-                
-            
-        dst_lev2 = dst_lev0.split('.cub')[0]+'_lev2.cub'     
-        if os.path.isfile(dst_lev2):
-            print('dst lev2 exists')
-        else:
-            try:
-                print('Map projecting')
-                dst_lev2 = cub2map(dst_lev0, maptemplate,dst_basename)    
-            except Exception as e:
-                print(e)
-        if os.path.isfile(dst_lev2.split('.cub')[0]+'.JP2'):
-            print('JP2 exists')
-        else:
-            map2jp2(dst_lev2)
-            #os.remove(dst_lev_x)
-        #os.remove(dst_lev0)
-
-        
         
 def equalizer(L2list):
     L2equ = [cub.split('.cub')[0]+'_equ.cub' for cub in L2list]
